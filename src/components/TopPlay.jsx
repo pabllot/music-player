358561bf059cc8e0255a12fba3c8b901643c62be
Pabllot07 <pabllot07@outlook.com>
2022-09-29 08:51:00 -0300

    topPlay impemented

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,44 @@
-const TopPlay = () => (
-  <div>TopPlay</div>
-);
+import { useEffect, useRef } from "react";
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from "react-redux";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode } from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
+
+import PlayPause from './PlayPause';
+import { playPause, setActiveSong } from '../redux/features/playerSlice'
+import { useGetTopChartsQuery } from '../redux/services/shazamCore'
+
+const TopPlay = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data } = useGetTopChartsQuery();
+  const divRef = useRef(null);
+
+  useEffect(() => {
+   divRef?.current.scrollIntoView({ behavior: 'smooth'});
+  });
+  
+
+  const topPlays = data?.slice(0, 5);
+
+  const handlePause = () => {
+    dispatch(playPause(false));
+    };
+    
+    const handlePlayClick = () => {
+      dispatch(setActiveSong({ song, data, i }));
+      dispatch(playPause(true));
+      console.log(activeSong)
+    };
+  
+  
+  return (
+    <div ref={divRef} className='xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col'></div>
+  )
+}
+
 
 export default TopPlay;
