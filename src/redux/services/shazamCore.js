b6d089cdfb01fd7d5e0c792a8a71a2a74fea2196
Pabllot07 <pabllot07@outlook.com>
2022-10-02 14:28:09 -0300

    select done

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -13,6 +13,7 @@ export const shazamCoreApi = createApi({
     }),
     endpoints: (builder) => ({
         getTopCharts: builder.query({ query: () => '/charts/world' }),
+        getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}`}),
         getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
         getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
         getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}`}),
@@ -22,6 +23,7 @@ export const shazamCoreApi = createApi({
 
 export const {
     useGetTopChartsQuery,
+    useGetSongsByGenreQuery,
     useGetSongDetailsQuery,
     useGetSongRelatedQuery,
     useGetArtistDetailsQuery,
